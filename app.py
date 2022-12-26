1e55e7dfe66ed42cb504677199eab88b104dafb1
Карпов Роман <90749981+Rominside@users.noreply.github.com>
2022-12-26 15:13:37 +0300

    Adds all my .py and .jinja files

--- app.py
+++ app.py
@@ -1,28 +1,137 @@
+import re
+
 from flask import Flask, render_template, request
-from database import Select_sidebar
+from database import Select_sidebar, Selection_dishes_search, Selection_dishes_tags, Update_insert_cart, Select_indents
+from database import Update_delete_cart, Authorization_verification, Update_assortment_insert, Select_assortment, Update_assortment_delete
+
+previous_search_result = []
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello world!<p>"
+@app.route("/Search2/Authorization/OwnerMode", methods=["GET", "POST"])
+def Owner_mode():
+    Colunm_list = ["Item_Number", "Supply_number", "Clients_number", "Product_type_number",
+                   "Product_material_number", "Dishes_stock_number", "Price", "Size",
+                   "Indent_number", "Provider_number"]
+    top_menu_activity = ["inactive", "active", "inactive"]
 
-
-@app.route("/Search")
-def Search():
-    sidebar = Select_sidebar()
-    item = ""
-    return render_template("dashboard.jinja", sidebar=sidebar)
-
-
-@app.route("/Search", methods=["POST", "GET"])
-def Search_requests():
     if request.method == "POST":
-        if request.form["Search"] == "dishes":
-            return "<p>Very good</p>"
+        if re.match(".+button_delete.+", str(request.form)):
+            Update_assortment_delete(request.form)
+        if re.match(".+button_insert.+", str(request.form)):
+            Update_assortment_insert(request.form)
+        if re.match(".+button_logout.+", str(request.form)):
+            return render_template("dashboard_2.jinja", top_menu_activity=top_menu_activity)
 
-    return render_template("dashboard.jinja")
+    return render_template("dashboard_2.jinja", top_menu_activity=top_menu_activity, OwnerMode=1,
+                           Assortment=Select_assortment(), Colunm_list=Colunm_list)
 
 
-# <p><input name={{item}} type="radio" value={{item}}></p>
+# @app.post("/Search2/Authorization")
+# def Users_post():
+# action="/Search2/Authorization/<login>"
+already_login = False
+
+
+@app.route("/Search2/Authorization", methods=["GET", "POST"])
+def hello_world():
+    global already_login
+    usrnm = ""
+    email = ""
+    psw =""
+    Colunm_list = ["Item_Number", "Supply_number", "Clients_number", "Product_type_number",
+                   "Product_material_number", "Dishes_stock_number", "Price", "Size",
+                   "Indent_number", "Provider_number"]
+    top_menu_activity = ["inactive", "active", "inactive"]
+    if request.method == "POST":
+        if "Login" in request.form:
+            usrnm = request.form.get("usrnm")
+            email = request.form.get("email")
+            psw = request.form.get("psw")
+            # return f'{usrnm}____{email}____{psw}'
+    link = "/Search2/Authorization/OwnerMode"
+    if request.method == "POST":
+        if re.match(".+button_logout.+", str(request.form)):
+            already_login = False
+            return render_template("dashboard_2.jinja", top_menu_activity=top_menu_activity)
+
+    if (usrnm == "FFFF F.F." and email == "FFFFFF@gmail.com" and psw == "80000000000") or already_login:
+        already_login = True
+        if request.method == "POST":
+            if re.match(".+button_delete.+", str(request.form)):
+                Update_assortment_delete(request.form)
+            if re.match(".+button_insert.+", str(request.form)):
+                Update_assortment_insert(request.form)
+
+        return render_template("dashboard_2.jinja", top_menu_activity=top_menu_activity, OwnerMode=1,
+                               Assortment=Select_assortment(), Colunm_list=Colunm_list, link=link)
+
+    return render_template("dashboard_2.jinja", Authorization=1, top_menu_activity=top_menu_activity, link="")
+
+
+# @app.route("/Search2/Authorization", methods=["GET", "POST"])
+# def Authorization():
+#     link = "/Search2/Authorization/<login>"
+#     top_menu_activity = ["inactive", "active", "inactive"]
+#     return render_template("dashboard_2.jinja", Authorization=1, top_menu_activity=top_menu_activity)
+
+
+@app.route("/Search2/Cart", methods=["GET", "POST"])
+def Cart():
+    top_menu_activity = ["inactive", "inactive", "active"]
+    sidebar = Select_sidebar()
+    if request.method == "POST":
+        if re.match(".+button_buy_[1-9]+.+", str(request.form)):
+            str_ = re.search("[1-9]+", str(request.form))
+            Update_delete_cart(str_[0])
+
+    return render_template("dashboard_2.jinja",
+                           dishes_search=Select_indents(), button_add_delete="Удалить из корзины",
+                           sidebar=sidebar, top_menu_activity=top_menu_activity)
+
+
+@app.route("/Search2")
+def Search2():
+    top_menu_activity = ["active", "inactive", "inactive"]
+    sidebar = Select_sidebar()
+    return render_template("dashboard_2.jinja", sidebar=sidebar, button_add_delete="Добавить в корзину",
+                           top_menu_activity=top_menu_activity)
+
+
+@app.route("/Search2", methods=["POST", "GET", "PUT"])
+def Search_requests2():
+    top_menu_activity = ["active", "inactive", "inactive"]
+    sidebar = Select_sidebar()
+    tags_list = []
+    global previous_search_result
+
+    if request.method == "POST":
+        if re.match(".+button_buy_[1-9]+.+", str(request.form)):
+            str_ = re.search("[1-9]+", str(request.form))
+            Update_insert_cart(str_[0])
+            return render_template("dashboard_2.jinja", sidebar=sidebar, button_add_delete="Добавить в корзину",
+                                   top_menu_activity=top_menu_activity, dishes_search=previous_search_result)
+
+    if request.method == "POST":
+        if "button_tags" in request.form:
+            for item_ in request.form:
+                if item_ != "button_tags":
+                    tags_list.append(item_)
+
+            previous_search_result = Selection_dishes_tags(tags_list, sidebar)
+            return render_template("dashboard_2.jinja",
+                                   dishes_search=Selection_dishes_tags(tags_list, sidebar),
+                                   sidebar=sidebar, top_menu_activity=top_menu_activity,
+                                   button_add_delete="Добавить в корзину")
+
+    if request.method == "POST":
+        if request.form["Search2"]:
+            previous_search_result = Selection_dishes_search(request.form["Search2"])
+            return render_template("dashboard_2.jinja",
+                                   dishes_search=Selection_dishes_search(request.form["Search2"]),
+                                   sidebar=sidebar, top_menu_activity=top_menu_activity,
+                                   button_add_delete="Добавить в корзину")
+
+    return render_template("dashboard_2.jinja", sidebar=sidebar, top_menu_activity=top_menu_activity,
+                           button_add_delete="Добавить в корзину")
--- database.py
+++ database.py
@@ -1,7 +1,10 @@
+import re
+from datetime import datetime, date, time
+
 import sqlalchemy
-from sqlalchemy import create_engine, inspect, select
+from sqlalchemy import create_engine, inspect, select, delete
 from sqlalchemy import MetaData, Table, Column
-from sqlalchemy import or_, and_, not_
+from sqlalchemy import or_, and_, not_, desc
 
 engine = create_engine("mysql+pymysql://root:Rom_544!@localhost:3306/my_db_dsh")
 engine.connect()
@@ -41,49 +44,224 @@ with engine.connect() as conn:
         print(row)
 
 
-def Select_dishes(search_request):
-    print("***************************")
-    # Select_dsh = select(dishes.c.Product_type_number).where(
-    #     dishes.c.Product_type_number == search_request
-    # )
-    # Select_dsh = select(type.c.Title).select_from(type.join(dishes)).where(
-    #     type.c.Title == search_request
-    # )
+def func_chunk(lst, n):
+    for x in range(0, len(lst), n):
+        e_c = lst[x : n + x]
+
+        if len(e_c) < n:
+            e_c = e_c + [None for y in range(n - len(e_c))]
+        yield e_c
+
+
+def Select_assortment():
+    assortment = select(dishes).select_from(dishes).where(dishes.c.Clients_number == 1)
+    select_assortment_list = Select_items_as_list(assortment)
+    final_select = func_chunk(select_assortment_list, 10)
+
+    return final_select
+
+
+def Update_assortment_delete(request):
+    dsh_number = request["Item_Number_delete"]
+    ass = delete(dishes).where(dishes.c.Item_Number == int(dsh_number))
+
+    with engine.connect() as conn_:
+        res = conn_.execute(ass)
+
+
+def Update_assortment_insert(request):
+
+    dsh = dishes.insert().values(
+        # Item_Number=request["Item_Number_insert"],
+        # Supply_number=request["Supply_number_insert"],
+        # Clients_number=request["Clients_number_insert"],
+        # Product_type_number=request["Product_type_number_insert"],
+        # Product_material_number=request["Product_material_number_insert"],
+        # Dishes_stock_number=request["Dishes_stock_number_insert"],
+        # Price=request["Price_insert"],
+        # Size=request["Size_insert"],
+        # Indent_number=request["Indent_number_insert"],
+        # Provider_number=request["Provider_number_insert"]
+        Item_Number=int(request["Item_Number_insert"]),
+        Supply_number=int(request["Supply_number_insert"]),
+        Clients_number=int(request["Clients_number_insert"]),
+        Product_type_number=int(request["Product_type_number_insert"]),
+        Product_material_number=int(request["Product_material_number_insert"]),
+        Dishes_stock_number=int(request["Dishes_stock_number_insert"]),
+        Price=int(request["Price_insert"]),
+        Size=int(request["Size_insert"]),
+        Indent_number=int(request["Indent_number_insert"]),
+        Provider_number=int(request["Provider_number_insert"])
+        # Item_Number=7,
+        # Supply_number=3,
+        # Clients_number=1,
+        # Product_type_number=3,
+        # Dishes_stock_number=2222,
+        # Price=890,
+        # Size=200,
+        # Indent_number=1,
+        # Provider_number=2
+    )
+
+    with engine.connect() as conn_:
+        res = conn_.execute(dsh)
+
+
+def Authorization_verification(search_login):
+    owner_verification = select(client.c.Full_name).select_from(client).\
+            where(
+                client.c.Contact_mail == search_login
+            )
+    select_authorization_list = []
+    with engine.connect() as conn:
+        result = conn.execute(owner_verification)
+        for tuple_ in result:
+            select_authorization_list.append(tuple_)
+    return select_authorization_list
+
+
+def Select_indents():
+    select_indent = select(indent.c.Indent_number, indent.c.Indent_price, indent.c.Product_type,
+                           indent.c.Product_material).\
+        select_from(indent).\
+        where(
+        indent.c.Clients_number == 4
+    )
+
+    select_indent_list = []
+    with engine.connect() as conn:
+        result = conn.execute(select_indent)
+        for tuple_ in result:
+            select_indent_list.append(tuple_)
+    return select_indent_list
+
+
+def Update_delete_cart(delete_dishes):
+    ins = delete(indent).where(
+        indent.c.Indent_number == delete_dishes
+    )
+
+    with engine.connect() as conn_:
+        res = conn_.execute(ins)
+
+
+def Update_insert_cart(add_dishes):
+    selected_item = select(dishes.c.Clients_number, dishes.c.Price, type.c.Title, material.c.Material_name).\
+        select_from(dishes.join(type).join(material)).\
+        where(
+            dishes.c.Item_Number == add_dishes
+    )
+    selected_item_list = Select_items_as_list(selected_item)
+
+    selected_max_number = select(indent.c.Indent_number).\
+        order_by(desc(indent.c.Indent_number)).limit(1)
+    selected_max_number_list = Select_items_as_list(selected_max_number)
+    max_indent_number = int(selected_max_number_list[0]) + 1
+
+    ins = indent.insert().values(
+        Indent_number=max_indent_number,
+        Clients_number=4,
+        Indent_date=datetime.date(datetime.now()),
+        Indent_price=selected_item_list[1],
+        Quantity_of_indent=1,
+        Product_type=selected_item_list[2],
+        Product_material=selected_item_list[3]
+    )
+    with engine.connect() as conn_:
+        res = conn_.execute(ins)
+
+
+def Selection_dishes_tags(tags_request, sidebar):
+    provider_list = []
+    material_list = []
+    type_list = []
+    for key_ in sidebar:
+        for item_sidebar in sidebar[key_]:
+            if item_sidebar in tags_request:
+                if key_ == "provider":
+                    provider_list.append(item_sidebar)
+                if key_ == "material":
+                    material_list.append(item_sidebar)
+                if key_ == "type":
+                    type_list.append(item_sidebar)
+
+    if len(provider_list) == 0:
+        S_p = select(provider.c.Company_name)
+        provider_list = Select_items_as_list(S_p)
+
+    if len(material_list) == 0:
+        S_m = select(material.c.Material_name)
+        material_list = Select_items_as_list(S_m)
+
+    if len(type_list) == 0:
+        S_t = select(type.c.Title)
+        type_list = Select_items_as_list(S_t)
+
+    Select_dishes_tags = select(dishes.c.Item_Number, type.c.Title, material.c.Material_name, provider.c.Company_name).\
+        select_from(dishes.join(type).join(material).join(provider)).\
+        where(
+            and_(
+                provider.c.Company_name.in_(provider_list),
+                type.c.Title.in_(type_list),
+                material.c.Material_name.in_(material_list)
+            )
+    )
+
+    dishes_list = []
+    with engine.connect() as conn:
+        result = conn.execute(Select_dishes_tags)
+        for tuple_ in result:
+            dishes_list.append(tuple_)
+    return dishes_list
+
+
+def Selection_dishes_search(search_request):
+    search_material = re.search("ceramics|porcelain|glass|plastic", search_request)
+    search_type = re.search("cup|plate|wineglass", search_request)
+    search_dishes = re.search("[1-9]+", search_request)
+
+    if search_material is not None:
+        str_material = str(search_material[0])
+    else:
+        str_material = "%%"
+    if search_type is not None:
+        str_type = str(search_type[0])
+    else:
+        str_type = "%%"
+    if search_dishes is not None:
+        str_dishes = str(search_dishes[0])
+    else:
+        str_dishes = "%%"
+
     Select_dsh = select(dishes.c.Item_Number, type.c.Title, material.c.Material_name).\
         select_from(dishes.join(type).join(material)).\
         where(
-            or_(
-                type.c.Title == search_request,
-                material.c.Material_name == search_request,
-                dishes.c.Item_Number == search_request
+            and_(
+                type.c.Title.like(str_type),
+                material.c.Material_name.like(str_material),
+                dishes.c.Item_Number.like(str_dishes)
             )
     )
+    dishes_list = []
     with engine.connect() as conn:
-        # return conn.execute(Select_dsh)
-        # print(conn.execute(Select_dsh))
         result = conn.execute(Select_dsh)
-        search_word = ""
-        for row in result:
-            for word in row:
-                search_word = search_word + str(word)
-            print(search_word)
-            search_word = ""
+        for tuple_ in result:
+            dishes_list.append(tuple_)
+    return dishes_list
 
 
-Select_dishes("cup")
-# , type.c.Title, , provider.c.Company_name
+Selection_dishes_search("cup")
 
 
 def Select_sidebar():
     select_sidebar_type = select(type.c.Title)
     select_sidebar_material = select(material.c.Material_name)
     select_sidebar_provider = select(provider.c.Company_name)
+    sidebar_dict = {"type": Select_items_as_list(select_sidebar_type),
+                    "material": Select_items_as_list(select_sidebar_material),
+                    "provider": Select_items_as_list(select_sidebar_provider)}
 
-    sidebar_list = [Select_items_as_list(select_sidebar_type),
-                    Select_items_as_list(select_sidebar_material),
-                    Select_items_as_list(select_sidebar_provider)]
-
-    return sidebar_list
+    return sidebar_dict
 
 
 def Select_items_as_list(select_result):
@@ -96,3 +274,4 @@ def Select_items_as_list(select_result):
 
 
 Select_sidebar()
+Select_indents()
